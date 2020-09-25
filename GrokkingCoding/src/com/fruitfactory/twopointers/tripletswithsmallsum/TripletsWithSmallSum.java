b2609f8202cf250b5109fbaefcde1f89be57ac65
Yaroslav Lisovskyi <yaroslav.lisovskyi@evry.com>
2020-09-25 22:02:11 +0300

    return all contineus array

--- GrokkingCoding/src/com/fruitfactory/twopointers/tripletswithsmallsum/TripletsWithSmallSum.java
+++ GrokkingCoding/src/com/fruitfactory/twopointers/tripletswithsmallsum/TripletsWithSmallSum.java
@@ -1,6 +1,8 @@
 package com.fruitfactory.twopointers.tripletswithsmallsum;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TripletsWithSmallSum {
     public int searchTriplets(int[] arr, int target) {
@@ -13,7 +15,6 @@ public class TripletsWithSmallSum {
 
         return count;
     }
-
 
     private int searchPair(int[] arr, int targetSum, int first){
         int count = 0;
@@ -28,6 +29,32 @@ public class TripletsWithSmallSum {
         }
         return count;
     }
+
+    public List<List<Integer>> returnTriplets(int[] arr, int target){
+        Arrays.sort(arr);
+        List<List<Integer>> triplets = new ArrayList<>();
+        for (int i = 0; i < arr.length - 2; i++){
+            searchPair(arr, target - arr[i], i, triplets);
+        }
+        return triplets;
+    }
+
+    private void searchPair(int[] arr, int targetSum, int first, List<List<Integer>> triplets){
+        int left = first + 1, right = arr.length - 1;
+
+        while(left < right){
+            if(arr[left] + arr[right] < targetSum){
+                for (int i = right; i > left; i-- ){
+                    triplets.add(Arrays.asList(arr[first],arr[left], arr[i]));
+                }
+                left++;
+            } else{
+                right--;
+            }
+        }
+    }
+
+
 
 }
 
